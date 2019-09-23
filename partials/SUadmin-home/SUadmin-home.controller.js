7a65aca3841182765b19e0b0c4b9841ace629761
Juan <juann.c.n@gmail.com>
2019-09-23 16:07:19 -0300

    Cambios en vista Super Administrador

--- partials/SUadmin-home/SUadmin-home.controller.js
+++ partials/SUadmin-home/SUadmin-home.controller.js
@@ -5,15 +5,35 @@
         .module('app')
         .controller('SUAdminHomeController', SUAdminHomeController);
 
-        SUAdminHomeController.$inject = ['UserService', '$rootScope', '$scope'];
+    SUAdminHomeController.$inject = ['UserService', '$rootScope', '$scope', '$uibModal'];
 
 
-    function SUAdminHomeController(UserService, $rootScope, $scope) {
+    function SUAdminHomeController(UserService, $rootScope, $scope, $uibModal) {
         var vm = this;
 
-       
+
+        $scope.openEdit = function () {
+            var modalInstance = $uibModal.open({
+                templateUrl: "partials/SUadmin-home/modal-edit.html",
+                controller: "partials/SUadmin-home/modal-edit.controller.js",
+                size: '',
+            });
+
+            modalInstance.result.then(function (response) {
+                $scope.result = `${response} button hitted`;
+            });
+        };
 
     }
+
+
+
+
+
+   
+
+
+
 
 
 })();
