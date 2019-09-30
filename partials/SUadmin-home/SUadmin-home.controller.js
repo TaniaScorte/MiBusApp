a84289fa73cc6f21bdb549d7a18296a45fa47d2c
Juan <juann.c.n@gmail.com>
2019-09-30 13:43:03 -0300

    crud empresa ok

--- partials/SUadmin-home/SUadmin-home.controller.js
+++ partials/SUadmin-home/SUadmin-home.controller.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular
+    var mi =angular
         .module('app')
         .controller('SUAdminHomeController', SUAdminHomeController);
 
@@ -11,9 +11,10 @@
     function SUAdminHomeController(UserService, $rootScope, $http, $scope, $uibModal) {
         var vm = this;
 
-        $scope.emp2 = 'hola';
+
+
+
         getEmpresas();
-       // getEmpresa();
 
 
         function getEmpresas() {
@@ -28,106 +29,140 @@
             $http(data)
                 .then(function (response) {
                     $scope.empresas = response.data;
-                    console.log(response.data);
+                         console.log(response.data);
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
         }
 
-        $scope.nueva = function(){
-            var url ='http://www.mellevas.com.ar/api/empresas/Create';   
-            var data ={
+        $scope.nueva = function () {
+            var url = 'http://www.mellevas.com.ar/api/empresas/Create';
+            var data = {
                 Nombre: $scope.txtNombreNew,
                 Direccion: $scope.txtDirNew,
                 Token: "2019",
             }
-   
-       var data = {
-           method: 'post',
-           url: url,
-           data:data
-          }
-          
-       $http(data)
-           .then(function(response){
-          console.log(response);
-           })
-           .catch(function(error){
-               console.log(error);
-           });
 
-
-       }
-
-
-        $scope.editar = function(id){
-             var url ='http://www.mellevas.com.ar/api/empresas/getEmpresa?id=';   
-    
-        var data = {
-            method: 'GET',
-            url: url + id + "&token=" + 2019,
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
+            var data = {
+                method: 'post',
+                url: url,
+                data: data
             }
-           }
-           
-        $http(data)
-            .then(function(response){
-           console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            });
+
+            $http(data)
+                .then(function (response) {
+                    console.log(response);
+                    getEmpresas();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
 
 
         }
 
-        $scope.eliminar = function(id){
-            var url ='http://www.mellevas.com.ar/api/empresas/getEmpresa?id=';   
-   
-       var data = {
-           method: 'GET',
-           url: url + id + "&token=" + 2019,
-           headers: {
-               'Content-Type': 'application/json; charset=utf-8'
-           }
-          }
-          
-       $http(data)
-           .then(function(response){
-          console.log(response);
-           })
-           .catch(function(error){
-               console.log(error);
-           });
 
+        $scope.editar = function (id) {
 
-       }
+            var id = parseInt(id);
 
+            var url = 'http://www.mellevas.com.ar/api/empresas/getEmpresa?id=';
 
-
-        /* var url ='http://www.mellevas.com.ar/api/empresas/getEmpresa?id=';   
-        var id = '1';
-    
-        var data = {
-            method: 'GET',
-            url: url + id + "&token=" + 2019,
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
+            var data = {
+                method: 'GET',
+                url: url + id + "&token=" + 2019,
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
             }
-           }
-           
-        $http(data)
-            .then(function(response){
-           console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            });
-*/
 
+            $http(data)
+                .then(function (response) {
+                    $scope.txtNombreEdit = response.data.Nombre;
+                    $scope.txtDirEdit = response.data.Direccion;
+                    // console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+
+
+
+            $('#btnEditar').on('click', function () {
+                var url = 'http://www.mellevas.com.ar/api/empresas/Update';
+
+                // console.log($scope.txtNombreEdit, $scope.txtDirEdit, id);
+
+                var data = {
+                    method: 'POST',
+                    url: url,
+                    id: id,
+                    Nombre: $scope.txtNombreEdit,
+                    Direccion: $scope.txtDirEdit,
+                    Token: "2019",
+                }
+
+                $http(data)
+                    .then(function (response) {
+                        console.log(response);
+                        getEmpresas();
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            });
+
+        }
+
+        $scope.eliminar = function (id) {
+
+            $('#btnEliminar').on('click', function () {
+                var url = 'http://www.mellevas.com.ar/api/empresas/Delete?id=';
+
+                var data = {
+                    method: 'GET',
+                    url: url + id + "&token=" + 2019,
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8'
+                    }
+                }
+
+                $http(data)
+                    .then(function (response) {
+                        getEmpresas();
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+            })
+
+
+        }
+
+
+
+        
+       
     }
+    mi.filter('filterDate', function(){
+        var cambiarFiltro = function(datosOriginales){
+
+
+           var nuevosDatos = datosOriginales.replace(/([A-Za-z)(\\/])/g,"");
+          //var nuevosDatos = datosOriginales.replace(/o/g,primerValor);
+         //var nuevosDatos = datosOriginales.replace(RegExp(segundoValor,"g"),primerValor);
+         //   var nuevosDatos = 'hola';
+
+            return nuevosDatos;
+
+        };
+
+        return cambiarFiltro;
+    });
 
 
 
