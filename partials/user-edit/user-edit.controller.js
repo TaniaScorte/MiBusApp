b361250d950c6badd7669b671d868ca6a049bad0
TaniaScorte <tania.scortegagna@gmail.com>
2019-10-30 01:34:37 -0300

    User edit funcion ok para todos los perfiles

--- partials/user-edit/user-edit.controller.js
+++ partials/user-edit/user-edit.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('UserEditController', UserEditController);
 
-        UserEditController.$inject = ['UserService','$location', '$rootScope','$scope','ResourcesService','$filter'];
-    function UserEditController(UserService, $location,$rootScope,$scope,ResourcesService,$filter) {
+        UserEditController.$inject = ['UserService','$location', '$rootScope','$scope','ResourcesService','$filter','SweetAlert'];
+    function UserEditController(UserService, $location,$rootScope,$scope,ResourcesService,$filter,SweetAlert) {
         var vm = $scope;
         initController();
 
@@ -37,22 +37,86 @@
         function loadEditUser(user) {
             vm.userEdit = {};
             vm.userEdit.tel = user.Telefono;
+            vm.userEdit.RolId = user.RolId;
             vm.userEdit.name = user.Nombre;
             vm.userEdit.surname = user.Apellido;
             vm.userEdit.dnitype =$filter('filter')($rootScope.dnitypes, {Id: user.TipoDni})[0];
-            vm.userEdit.password = user.Clave;
             vm.userEdit.dni = user.Dni;
             vm.userEdit.email= user.Email;
+            vm.userEdit.password = user.Clave;
+            vm.userEdit.tel= Number(user.Telefono);
         }
-      /*  function loadAllUsers() {
-            UserService.GetAll()
-                .then(function (users) {
-                    vm.allUsers = users;
+        vm.updateUser = function (userEdit) {
+            if(validarUserPassword(userEdit.password,userEdit.confirmPassword)){
+                var data ={
+                    Id: vm.$parent.user.Id,
+                    Nombre: userEdit.name,
+                    Apellido: userEdit.surname,
+                    TipoDni: userEdit.dnitype.Id,
+                    Dni: userEdit.dni,
+                    Email: userEdit.email,
+                    Clave: userEdit.password,
+                    Telefono: userEdit.tel,
+                    EmpresaId: vm.$parent.user.EmpresaId,
+                    RolId:vm.$parent.user.RolId
+                }          
+                UserService.Update(data)
+                .then(function (response) {
+                    if (response.Estado == 0){
+                        SweetAlert.swal ({
+                            type: "success", 
+                            title: "La operacion se ha realizado con exito",
+                            text: "El usuario ha sido actualizado",
+                            confirmButtonAriaLabel: 'Ok',
+                        });
+                        vm.dataLoading = false;
+                        return;
+                    } 
+                    else {
+                        vm.dataLoading = false;
+                        SweetAlert.swal ({
+                            type: "error", 
+                            title: "Error",
+                            text: "Error al actualizar el usuario",
+                            confirmButtonAriaLabel: 'Ok',
+                        });
+                    }
+                })
+                .catch(function(error){
+                    vm.dataLoading = false;
+                    SweetAlert.swal ({
+                        type: "error", 
+                        title: "Error",
+                        text: error,
+                        confirmButtonAriaLabel: 'Ok',
+                    });
+                });  
+            }
+        }
+        function validarUserPassword(pass, confirm){
+            if(pass==null || pass==undefined || confirm == "" || confirm==undefined || confirm== ""){
+                vm.dataLoading = false;
+                SweetAlert.swal ({
+                    type: "warning", 
+                    title: "Confirme la clave",
+                    text: "Verifique que los campos esten correctos",
+                    confirmButtonAriaLabel: 'Ok',
                 });
-        }
-*/
-        function deleteUser(id) {
-                    
+                return false;
+            }
+            if(!(pass === confirm)){
+                vm.dataLoading = false;
+                SweetAlert.swal ({
+                    type: "warning", 
+                    title: "Confirme el password",
+                    text: "Verifique que los campos esten correctos",
+                    confirmButtonAriaLabel: 'Ok',
+                });
+                return false;
+            }
+            else{
+                return true;
+            }
         }
     }
 
