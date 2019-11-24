86a1b0fdf2b1a237f6950becdc2ee62e0ee254c4
TaniaScorte <tania.scortegagna@gmail.com>
2019-11-23 21:04:52 -0300

    Ajuste en la compra

--- partials/buy/buy.controller.js
+++ partials/buy/buy.controller.js
@@ -8,18 +8,36 @@
         BuyController.$inject = ['$location','$scope','$rootScope','MapResourcesService','ResourcesService','SweetAlert'];
     function BuyController($location,$scope,$rootScope,MapResourcesService,ResourcesService,SweetAlert) {
         var vm = $scope;
-        vm.paramsBuy = $rootScope.paramsBuy;
-
         initController();
 
         function initController() {
-            //loadCurrentUser();
-           // loadAllUsers();
+            if(!$rootScope.paramsBuy){
+                $location.path('/schedules');
+                return;
+            }
+            vm.paramsBuy = $rootScope.paramsBuy;
+            getParadasByRecorrido();
         }
 
         vm.backSchedules = function(){
             $rootScope.backSchedules = true;
             $location.path('/schedules');
+        }
+        function getParadasByRecorrido() {
+            ResourcesService.GetParadasByRecorrido(vm.paramsBuy.recorrido.Id)
+            .then(function (response) {
+                if (response){
+                   $rootScope.busStops = response;       
+                } 
+            })
+            .catch(function(error){
+                SweetAlert.swal ({
+                    type: "error", 
+                    title: "Error",
+                    text: error,
+                    confirmButtonAriaLabel: 'Ok',
+                });
+            });
         }
     }
 
